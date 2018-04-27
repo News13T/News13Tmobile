978761fc8c6a683cfd70765e248c8535661fa6c6
News13T <mviaud85@gmail.com>
2018-04-27 15:28:20 +0200

    modification de la page html user

---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { UserResolver } from './user/user.resolver';
+import { AuthGuard } from './core/auth.guard';
+import { AuthService } from './core/auth.service';
+import { UserService } from './core/user.service';
+import { rootRouterConfig } from './app.routes';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePage,
+    ListPage,
+    LoginComponent,
+    UserComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    IonicModule.forRoot(AppComponent),
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    AppComponent,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    AuthService, 
+    UserService, 
+    UserResolver, 
+    AuthGuard,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../core/auth.service'
+import { Router, Params } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: 'register.component.html',
+  styleUrls: ['/src/app/register/register.component.scss']
+})
+export class RegisterComponent {
+
+  registerForm: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+   }
+
+   createForm() {
+     this.registerForm = this.fb.group({
+       email: ['', Validators.required ],
+       password: ['',Validators.required]
+     });
+   }
+
+   tryFacebookLogin(){
+     this.authService.doFacebookLogin()
+     .then(res =>{
+       this.router.navigate(['/user']);
+     }, err => console.log(err)
+     )
+   }
+
+   tryTwitterLogin(){
+     this.authService.doTwitterLogin()
+     .then(res =>{
+       this.router.navigate(['/user']);
+     }, err => console.log(err)
+     )
+   }
+
+   tryGoogleLogin(){
+     this.authService.doGoogleLogin()
+     .then(res =>{
+       this.router.navigate(['/user']);
+     }, err => console.log(err)
+     )
+   }
+
+   tryRegister(value){
+     this.authService.doRegister(value)
+     .then(res => {
+       console.log(res);
+       this.errorMessage = "";
+       this.successMessage = "Votre compte a été créé";
+       this.router.navigate(['/user']);
+     }, err => {
+       console.log(err);
+       this.errorMessage = err.message;
+       this.successMessage = "";
+     })
+   }
+
+}
